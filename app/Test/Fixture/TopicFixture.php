41caee896274335066a6d4c117e45345e9b0c5ac
riditalove <fabihaafrose69@gmail.com>
2025-03-16 08:27:39 +0600

    commit

---
+++ app/Test/Fixture/TopicFixture.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Topic Fixture
+ */
+class TopicFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'unsigned' => false, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'unsigned' => false),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'visible' => array('type' => 'boolean', 'null' => false, 'default' => null, 'comment' => '1 for visible, 2 for hidden'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'user_id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'created' => '2025-03-15 20:41:16',
+			'modified' => '2025-03-15 20:41:16',
+			'visible' => 1
+		),
+	);
+
+}
---
+++ app/Test/Fixture/UserFixture.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User Fixture
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'unsigned' => false, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 20, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'full_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'role' => array('type' => 'boolean', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'username' => 'Lorem ipsum dolor ',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'full_name' => 'Lorem ipsum dolor sit amet',
+			'role' => 1,
+			'created' => '2025-03-15 20:41:40',
+			'modified' => '2025-03-15 20:41:40'
+		),
+	);
+
+}
